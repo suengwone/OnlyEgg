c5f95a3dfb9ce2a961f86f0cd886e9211af6f0cc
Ethan <suengwone@gmail.com>
2024-12-17 18:39:35 +0900

    🔨 Time Manager that only manage time things

--- Assets/Scripts/Main/Manager/TimeManager.cs
+++ Assets/Scripts/Main/Manager/TimeManager.cs
@@ -1,42 +1,11 @@
-using System;
-using System.Collections;
-using UnityEngine;
-
 public class TimeManager : BaseMonoBehaviour
 {
-    public IEnumerator FlowTimeInRealTime(Action onFinish)
+    public void PauseGame()
     {
-        WaitForSecondsRealtime secondRealTime = new WaitForSecondsRealtime(1);
 
-        while (true)
-        {
-            // second -= 1;
+    }
+    public void ResumeGame()
+    {
 
-            // if (second < 0)
-            // {
-            //     second += 60;
-            //     minute -= 1;
-
-            //     if (minute < 0)
-            //     {
-            //         minute += 60;
-            //         hour -= 1;
-
-            //         if (hour < 0)
-            //         {
-            //             hour = 0;
-            //             minute = 0;
-            //             second = 0;
-            //             break;
-            //         }
-            //     }
-            // }
-
-            // uiManager.UpdateTime();
-
-            yield return secondRealTime;
-        }
-
-        onFinish?.Invoke();
     }
 }
